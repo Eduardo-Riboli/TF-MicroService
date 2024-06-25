86647a5f97bb31c58e0638a6e64fb11f49061ec7
duduagarcia <eduardo.agarcia117@gmail.com>
2024-06-25 13:53:18 -0300

    fix: connecting cache to name server

--- cache-service/src/main/java/com/engsoft2/cache_service/CacheServiceApplication.java
+++ cache-service/src/main/java/com/engsoft2/cache_service/CacheServiceApplication.java
@@ -1,13 +1,25 @@
 package com.engsoft2.cache_service;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CacheServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CacheServiceApplication.class, args);
+	}
+
+	@GetMapping("/getStatus")
+	public HashMap<String, String> getStatus(){
+		HashMap<String, String> status = new HashMap<>();
+		status.put("status", "running");
+		return status;
 	}
 
 }
